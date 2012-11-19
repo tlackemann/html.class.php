66ae6ea9b2e4a75641556a5711cd029d8a7cc461
Thomas Lackemann <tommylackemann@gmail.com>
2012-11-19 13:47:03 -0500

    beginning form helpers

--- html.class.php
+++ html.class.php
@@ -1,15 +1,52 @@
 <?php
 /**
- * @package
+ * @package html.class.php
  *
  * @author Thomas Lackemann
  *
- * Adapted from tenantmls-php
+ * Originally adapted from tenantmls-php
  * https://github.com/tlackemann/tenantmls-php
  *  
  */
 
 class HTML {
+
+  /**
+   * Universal Options
+   * =================
+   * 
+   * Most elements take a set of options and are
+   * modified using an options array
+   *
+   * DEFAULTS:
+   *   ID     => ''   #id attribute of the element
+   *   CLASS  => ''   #class attribute of the element
+   *   NAME   => ''   #name attribute of the element
+   *   TITLE  => ''   #title attribute of the element
+   *   REL    => ''   #rel attribute of the element
+   *
+   */
+  protected function _process_options($options = array()) {
+    $_options = array('ID','CLASS','REL','TITLE','NAME','FOR');
+    $data = '';
+    foreach($options as $option => $value) {
+      if (in_array(strtoupper($option), $_options)) {
+        $data.= ' '.strtolower($option)].'="'.$value.'"';
+      }
+    }
+    return $data;
+  }
+
+  /**
+   * show_php_errors()
+   * displays any non-fatal errors (set false for live environment)
+   */
+  function show_php_errors($option = false) {
+    if ($option) {
+      error_reporting(E_ALL);
+      ini_set('display_errors', '1');
+    }
+  }
 
   function link_to($text, $path, $class = '', $id = '', $prompt = null,$confirmMessage = "Are you sure?") {
     $path = str_replace(' ','+',$path);
@@ -42,32 +79,60 @@ class HTML {
     return $data;
   }
 
-  function formSelect($name = null, $values = array(), $selected = null, $id = null, $class = null) {
-    if ($name) $displayName = ' name="'.$name.'"'; else $displayName = '';
-    if ($id) $displayId = ' id="'.$id.'"'; else $displayId = '';
-    if ($class) $displayClass = ' class="'.$class.'"'; else $displayClass = '';
+  /**
+   *
+   * Form Helpers
+   * ============
+   *
+   * Example:
+   *
+   *    $elements = array(
+   *      $html->select_list('element',array('a','b','c'))
+   *    );
+   *    $html->make_form('name',$elements);
+   *
+   * Outputs:
+   *
+   *    <form name="name" method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+   *      <select name="element">
+   *        <option value="0">a</option>
+   *        <option value="1">b</option>
+   *        <option value="2">c</option>
+   *      </select>
+   *    </form>
+   *
+   */
 
-    $data = '<select'.$displayName.$displayId.$displayClass.'>';
-    foreach($values as $value => $name) {
-      if ((string) $value==(string) $selected) {
-        $data.= '<option value="'.$value.'" selected >'.$name.'</option>';
-      } else {
-        $data.= '<option value="'.$value.'">'.$name.'</option>';
-      }
-    }
-    $data.= '</select>';
+  /**
+   * make_form([$name[,$elements[,$options]]])
+   *   $name = name of the form
+   *   $elements = array containing the HTML of each element
+   *   $options = universal options array
+   */
+  function make_form($name = '', $elements = array(), $options = array()) {
+    $option = $this->_build_options($options);
+    $data = "<form name=\"$name\"$option>";
 
-    return $data;
+    $data.= "</form>";
+
+    return $form;
   }
 
-  function formInput($type = 'text', $name = null, $value = null, $size = null, $id = null, $class = null) {
-    if ($name) $displayName = ' name="'.$name.'"'; else $displayName = '';
-    if ($id) $displayId = ' id="'.$id.'"'; else $displayId = '';
-    if ($class) $displayClass = ' class="'.$class.'"'; else $displayClass = '';
-    if ($value) $displayValue = ' value="'.$value.'"'; else $displayValue = '';
-    if ($size) $displaySize = ' size="'.$size.'"'; else $displaySize = '';
-
-    $data = "<input type=\"$type\"$displayName$displayId$displayClass$displayValue$displaySize />";
+  /**
+   * select_list([$name[,$elements[,$selected[,$options]]]])
+   *   $name = name of the form
+   *   $elements = array containing the HTML of each element
+   *   $options = universal options array
+   */
+  function select_list($name = '', $elements = array(), $selected = false, $options = array()) {
+    $option = $this->_build_options($options);
+    $data = "<select name=\"$name\"$option>";
+    foreach($elements as $k => $v) {
+      $s = ($selected == $k) ? ' selected="selected"' : '';
+      $data.= "<option value=\"$k\"$s>$v</option>";
+    }
+    $data.= '</select>';
+    
     return $data;
   }
 }
